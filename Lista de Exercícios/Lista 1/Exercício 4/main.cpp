2d6e95e0391598f712c5dd3fc240fd0730d8d544
brunounky <bruno_unky@hotmail.com>
2022-09-17 11:35:32 -0300

    desenvolvimento do exercicio 4

---
+++ Lista de Exercícios/Lista 1/Exercício 4/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <locale>
+#include <stdio.h>
+
+using namespace std;
+
+//nao esta funcionando
+
+int main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	cout << "EXERCICIO 4" << endl;
+	
+	char t[3];
+	
+	cout << "Digite SIM ou NAO para receber um retorno: " << endl;
+	cin >> t;
+	
+	if(t=="sim"){
+		cout << "Retorno: 1" << endl;
+	}if(t=="nao"){
+		cout << "Retorno: 0" << endl;
+	}else{
+		cout << "Você não digitou uma das opções possíveis" << endl;
+		cout << t << endl;
+	}
+	
+	
+	return 0;
+}
