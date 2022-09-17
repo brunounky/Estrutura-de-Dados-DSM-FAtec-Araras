0ca06da13d495e632eee81c04100d75b114df3b1
brunounky <bruno_unky@hotmail.com>
2022-09-16 23:53:38 -0300

    desenvolvimento do exercicio 2

---
+++ Lista de Exercícios/Lista 1/Exercício 2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <locale>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+
+//Funciona, verificar se está como o exercício pede
+
+int main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	int i;
+	double quant[10];
+	
+	cout << "CORRETOR DE PREÇOS" << endl;
+	
+	for(i = 1; i<=10; i++){
+		cout << "Informe o valor do produto " << i << endl;
+		cin >> quant[i];
+	}
+	
+	for(i = 1; i<=10; i++){
+		cout << "O valor do produto: " << i << " corrigido pela inflação é de: " << (quant[i]/100*15)+quant[i] << endl;
+	}
+	
+	return 0;
+}
