fcf8f3c08c1956174c4c293292e5280bdc423f73
brunounky <bruno_unky@hotmail.com>
2022-09-17 10:41:42 -0300

    desenvolvimento do exercicio 3

--- Lista de Exercícios/Lista 1/Exercício 3/main.cpp
+++ Lista de Exercícios/Lista 1/Exercício 3/main.cpp
@@ -9,9 +9,27 @@ int main() {
 	
 	setlocale(LC_ALL, "Portuguese");
 	
+	int soma, i, conj[10];
+	float final;
 	
+	cout << "EXERCÍÍO 3" << endl;
 	
+	for(i = 0; i<10; i++){
+		cout << "Informe o valor " << i+1 << " : " << endl;
+		cin >> conj[i];
+		
+		soma=soma+conj[i];
+	}
 	
+		final=soma/10;
+
+		cout << endl << "A média dos conjuntos é: " << final << endl;
+		
+		for(i=0; i<10; i++){
+			if(final==conj[i]){
+				cout << "o valor da posição: " << i+1 << " é igual a média de todos os conjuntos" << endl;
+			}
+		}
 	
 	return 0;
 }
