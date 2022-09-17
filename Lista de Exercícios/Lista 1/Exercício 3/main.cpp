90870745bdc2a5d8968c52aae7a47a325d6f208f
brunounky <bruno_unky@hotmail.com>
2022-09-17 09:39:06 -0300

    desenvolvimento do exercicio 2

---
+++ Lista de Exercícios/Lista 1/Exercício 3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <locale>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	
+	
+	
+	
+	return 0;
+}
