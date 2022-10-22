cb3eadbbb467a0ff540aed691526091c934eca9c
brunounky <bruno_unky@hotmail.com>
2022-10-21 23:58:32 -0300

    Desenvolvimento de exercícios

--- Lista de Exercícios/Lista 1/Exercício 1/main.cpp
+++ Lista de Exercícios/Lista 1/Exercício 1/main.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <locale>
 #include <cmath>
-#include <stdlib.h>
 #include <stdio.h>
 
 using namespace std;
 
-int main(void) {
-	
-	setlocale(LC_ALL, "Portuguese");
-	
-	int i;
-	int k=1;
-	
-	printf("\t TABUADA DO 1 AO 9 \n");
-	
-	for(int i=0; i<10; i++){
-		
-	printf( "\n 1*%i=", i+1*k+1);
-		
-	}
-	
-	return 0;
+int main()
+{
+  int x,y;
+  for(x=2; x <= 10; x++)
+  {
+     for(y=1; y <= 10; y++) {
+        if (y == 10) {
+           cout << x << "  * " << y << "  = " << x*y << endl;
+        } else {
+           cout << x << "  *  " << y << "  = " << x*y << endl;
+        }
+     }
+     cout << endl;
+  }
+  system("pause");
 }
