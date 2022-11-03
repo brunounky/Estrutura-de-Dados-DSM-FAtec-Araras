d13dae10440be9778cad1f51872ef8efa115657a
brunounky <bruno_unky@hotmail.com>
2022-11-03 11:38:44 -0300

    Exercício 1

---
+++ Lista de Exercícios/Lista 2/Exercício 1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <locale>
+#include <cmath>
+#include <stdio.h>
+
+
+using namespace std;
+
+struct contaCorrente{
+	
+	char nome[30];
+	int cpf[11];
+	float valorConta;
+	
+};
+
+int main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+		
+	
+		
+	return 0;
+}
