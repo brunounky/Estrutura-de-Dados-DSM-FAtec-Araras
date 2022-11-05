1cb6978ec40320a0b138aeff7181d447a4ca1eed
brunounky <bruno_unky@hotmail.com>
2022-11-04 22:11:21 -0300

    Exercício 1 da Lista 2

--- Lista de Exercícios/Lista 2/Exercício 1/main.cpp
+++ Lista de Exercícios/Lista 2/Exercício 1/main.cpp
@@ -3,22 +3,63 @@
 #include <cmath>
 #include <stdio.h>
 
+//Lista 2 - Exercício 1
+
 
 using namespace std;
 
 struct contaCorrente{
-	
+	                      
 	char nome[30];
-	int cpf[11];
+	int cpf;
 	float valorConta;
 	
 };
 
+void cadastrarContas(){
+	
+	int i;
+	contaCorrente Contas[5]; //vetor para cadastrar no novo tipo
+
+for(i=1; i<=5; i++){
+		
+	cout << "_______________________________________________________________" << endl;
+	
+	cout << "Informe um nome para a criação de uma nova conta bancária: " << i << endl;
+	cin >> Contas[i].nome;
+	
+	cout << endl;
+	
+	cout << "Informe o CPF do novo correntista: " << i << endl;
+	cin >> Contas[i].cpf;
+	
+	cout << endl;
+	
+	cout << "Informe o valor inicial dessa conta: " << i << endl;
+	cin >> Contas[i].valorConta;
+	
+	cout << endl;
+	
+	}
+	
+		for(i=1; i <=5; i++){
+		
+		cout << "______________________________________________" << endl;
+		cout << "Nome do correntista " << i << " : " << Contas[i].nome << endl;
+		cout << "CPF do correntista " << i << " : " << Contas[i].cpf << endl;
+		cout << "Valor na conta do correntista " << i << " : " << "R$" << Contas[i].valorConta << endl; 
+		
+	}
+	
+}
+
 int main() {
 	
 	setlocale(LC_ALL, "Portuguese");
-		
 	
+	cout << "Banco Fatec Araras" << endl;
+	
+	cadastrarContas();
 		
 	return 0;
 }
