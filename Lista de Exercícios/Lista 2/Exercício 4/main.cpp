dcd21d0cdaec73aebafb306916c886271fc8bbae
brunounky <bruno_unky@hotmail.com>
2022-11-05 15:35:27 -0300

    Desenvolvimento do Exercício 4

--- Lista de Exercícios/Lista 2/Exercício 4/main.cpp
+++ Lista de Exercícios/Lista 2/Exercício 4/main.cpp
@@ -3,15 +3,72 @@
 #include <cmath>
 #include <stdio.h>
 
-//Lista 2 - Exercício 3
+//Lista 2 - Exercício 4
 
 
 using namespace std;
 
+struct pontosTriangulo1{
+
+	 double x[3],y[3];
+
+};
+
+struct pontosTriangulo2{
+
+	 double x[3],y[3];
+
+};
+
+void qualTipo(){
+
+	int i, j;
+	pontosTriangulo1 PontosTriangulo1;
+	pontosTriangulo1 PontosTriangulo2;
+	
+	cout << "Qual o tipo de triângulo" << endl;
+	
+	cout << "________________________________________________" << endl;
+	
+	for(j=1; j<=3; j++){ //contador de pontos para cada triangulo
+			
+			cout << "Informe o valor de X do ponto " << j << " do trinagulo 1" << endl;
+			cin >> PontosTriangulo1.x[j];
+			
+			cout << "Informe o valor de Y do ponto " << j << " do trinagulo 1" << endl;
+			cin >> PontosTriangulo1.y[j];	
+	}
+	
+	cout << "________________________________________________" << endl;
+	
+	for(i=1; i<=3; i++){ //contador de pontos para cada triangulo
+			
+			cout << "Informe o valor de X do ponto " << i << " do trinagulo 2" << endl;
+			cin >> PontosTriangulo2.x[i];
+			
+			cout << "Informe o valor de Y do ponto " << i << " do trinagulo 2" << endl;
+			cin >> PontosTriangulo2.y[i];	
+	}
+	
+	for(i=1; i<=3; i++){
+	cout << PontosTriangulo1.x[i] << endl;
+	cout << PontosTriangulo1.y[i] << endl;
+	}
+	
+	for(j=1; j<=3; j++){
+	cout << PontosTriangulo2.x[j] << endl;
+	cout << PontosTriangulo2.y[j] << endl;
+	}
+	
+	
+}
+
 
 int main() {
 	
+	setlocale(LC_ALL, "Portuguese");
 	
-	
+	qualTipo();
+
 	return 0;
 }
