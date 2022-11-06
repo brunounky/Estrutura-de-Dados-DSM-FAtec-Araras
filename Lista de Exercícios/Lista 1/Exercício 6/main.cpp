295f9ab19c00167f932017c6bb20036650a0ce40
brunounky <bruno_unky@hotmail.com>
2022-11-06 01:39:24 -0300

    Desenvolvimento do exercício 6 da lista 1

--- Lista de Exercícios/Lista 1/Exercício 6/main.cpp
+++ Lista de Exercícios/Lista 1/Exercício 6/main.cpp
@@ -3,14 +3,36 @@
 #include <cmath>
 #include <stdio.h>
 
+//Lista 1 - Exercício 6
+
 using namespace std;
 
+int fibonacci(int d){
+	
+	int antD = 0;
+	int i, aux;
+	
+	cout << "A sequência é: " << endl << d << endl;
+	
+	for(i=1; i<15; i++){
+		
+		cout << d+antD << endl;
+		
+		aux = antD;
+		antD = d;
+		d = d + aux;
+		
+	}
+	
+}
 
 int main() {
 	
 	setlocale(LC_ALL, "Portuguese");
 	
+	int d=1;
 	
+	fibonacci(d);
 	
 	return 0;
 }
