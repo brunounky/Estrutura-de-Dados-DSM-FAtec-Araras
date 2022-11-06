1b81cc373f5ed2ff89f1315281d2344b266ff216
brunounky <bruno_unky@hotmail.com>
2022-11-06 11:16:13 -0300

    Finalização do exercício 3 da lista 1

--- Lista de Exercícios/Lista 1/Exercício 3/main.cpp
+++ Lista de Exercícios/Lista 1/Exercício 3/main.cpp
@@ -2,14 +2,38 @@
 #include <locale>
 #include <cmath>
 #include <stdio.h>
+#include <cstring>
+
+//Lista 1 - Exercício 3
 
 using namespace std;
+
+void verifica(int conj[], float final){
+	
+	int i;
+	float j=0;
+	
+	for(i=0; i<10; i++){
+		if(final==conj[i]){
+			cout << "o valor da posição " << i+1 << " é igual a média de todos os conjuntos" << endl;
+		}else{
+			j=1;
+		}
+	}
+	
+	if(j==1){
+		cout << endl;
+		cout << "Nenhum dos outros valores inseridos é igual ao valor da média" << endl;
+	}
+	
+	
+}
 
 int main() {
 	
 	setlocale(LC_ALL, "Portuguese");
 	
-	float soma, final, j=0;
+	float soma, final;
 	int i, conj[10];
 	
 	cout << "EXERCÍÍO 3" << endl;
@@ -25,19 +49,8 @@ int main() {
 
 		cout << endl << "A média dos conjuntos é: " << final << endl;
 		
-	for(i=0; i<10; i++){
-		if(final==conj[i]){
-			cout << "o valor da posição " << i+1 << " é igual a média de todos os conjuntos" << endl;
-		}else{
-			j=1;
-		}
-	}
-	
-	if(j==1){
-		cout << endl;
-		cout << "Nenhum dos outros valores inseridos é igual ao valor da média" << endl;
-	}
-		
+	verifica(conj, final);
+			
 	
 	return 0;
 }
