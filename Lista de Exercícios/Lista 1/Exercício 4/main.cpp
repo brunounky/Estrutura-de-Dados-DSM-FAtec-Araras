2cbb47fedb98a92258606435b32b9dcd671ee003
brunounky <bruno_unky@hotmail.com>
2022-11-06 03:35:33 -0300

    Tentativa de desenvolvimento do exercício

--- Lista de Exercícios/Lista 1/Exercício 4/main.cpp
+++ Lista de Exercícios/Lista 1/Exercício 4/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <locale>
+#include <cmath>
 #include <stdio.h>
+#include <cstring>
+
+//Lista 1 - Exercício 4
 
 using namespace std;
 
-//nao esta funcionando
+void retorno(char t){
+	
+	if(t == 's'){
+		cout << "Retorno: 1" << endl;
+	}if(t == 'n'){
+		cout << "Retorno: 0" << endl;
+	}else{
+		cout << "Você não digitou uma das opções possíveis" << endl;
+	}
+	
+}
 
 int main() {
 	
@@ -12,20 +26,12 @@ int main() {
 	
 	cout << "EXERCICIO 4" << endl;
 	
-	char t[3], sim, nao;
+	char t;
 	
 	cout << "Digite SIM ou NAO para receber um retorno: " << endl;
 	cin >> t;
 	
-	if(t=="sim"){
-		cout << "Retorno: 1" << endl;
-	}if(t=="nao"){
-		cout << "Retorno: 0" << endl;
-	}else{
-		cout << "Você não digitou uma das opções possíveis" << endl;
-		cout << t << endl;
-	}
-	
+	retorno(t);
 	
 	return 0;
 }
